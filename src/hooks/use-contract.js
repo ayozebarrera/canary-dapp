876d89f5918f8521283a9e7804e596b283dfd3c5
Ayoze Barrera <ayozebarrera@gmail.com>
2022-05-05 18:37:37 +0100

    Update contract

--- src/hooks/use-contract.js
+++ src/hooks/use-contract.js
@@ -1,13 +1,12 @@
-import { ethers } from 'ethers';
-import { useEffect, useState } from 'react';
-import ContractJson from '../utils/Canary.json';
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import ContractJson from "../utils/Canary.json";
 
 const CanaryDetails = {
-  // TODO 1: Replace the address with the address of the deployed contract
-  CONTRACT_ADDRESS: '0xd44aBFB64B98C13a69839BFF433814446726703A',
+  CONTRACT_ADDRESS: "0xF6c4BE6aEeE3a161a5aFB456EF2bf642C9764CF6",
   // If you have changed the contract code, you will need to update the ABI file
   CONTRACT_ABI: ContractJson.abi,
-};  
+};
 
 export function useContract(ethereum, isValidChain) {
   const [contract, setContract] = useState(null);
@@ -18,7 +17,11 @@ export function useContract(ethereum, isValidChain) {
     // We initialize the contract with the provider and the ABI only once
     const provider = new ethers.providers.Web3Provider(ethereum);
     const signer = provider.getSigner();
-    const newContract = new ethers.Contract(CanaryDetails.CONTRACT_ADDRESS, CanaryDetails.CONTRACT_ABI, signer);
+    const newContract = new ethers.Contract(
+      CanaryDetails.CONTRACT_ADDRESS,
+      CanaryDetails.CONTRACT_ABI,
+      signer
+    );
     setContract(newContract);
   }, [ethereum, contract, isValidChain]);
   return contract;
